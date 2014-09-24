4f6b7070f5088ea12b84d42e02a8a0da8e1b6545
Jonas Falck <jonas.falck@gmail.com>
2014-09-25 00:19:38 +0200

    fixed tests

--- goenocean_test.go
+++ goenocean_test.go
@@ -8,8 +8,8 @@ func TestEncode(t *testing.T) {
 	p.SetData([]byte{0x01, 0x02, 0x03})
 	p.SetOptData([]byte{0x04, 0x05, 0x06})
 	encoded := p.Encode()
-	if toHex(encoded) != "55 00 03 03 00 82 01 02 03 04 05 06 2f" {
-		t.Errorf("encoding failed: %s", toHex(encoded))
+	if ToHex(encoded) != "55 00 03 03 00 82 01 02 03 04 05 06 2f" {
+		t.Errorf("encoding failed: %s", ToHex(encoded))
 	}
 }
 func TestEncodeCO_WR_RESET(t *testing.T) {
@@ -18,8 +18,8 @@ func TestEncodeCO_WR_RESET(t *testing.T) {
 	p.SetPacketType(0x05)
 	p.SetData([]byte{0x02})
 	encoded := p.Encode()
-	if toHex(encoded) != "55 00 01 00 05 70 02 0e" {
-		t.Errorf("encoding failed: %s", toHex(encoded))
+	if ToHex(encoded) != "55 00 01 00 05 70 02 0e" {
+		t.Errorf("encoding failed: %s", ToHex(encoded))
 	}
 }
 func TestEncodeCO_RD_IDBASE(t *testing.T) {
@@ -27,8 +27,8 @@ func TestEncodeCO_RD_IDBASE(t *testing.T) {
 	p.SetPacketType(0x05)
 	p.SetData([]byte{0x08})
 	encoded := p.Encode()
-	if toHex(encoded) != "55 00 01 00 05 70 08 38" {
-		t.Errorf("encoding failed: %s", toHex(encoded))
+	if ToHex(encoded) != "55 00 01 00 05 70 08 38" {
+		t.Errorf("encoding failed: %s", ToHex(encoded))
 	}
 }
 func TestSetGetPacketType(t *testing.T) {
