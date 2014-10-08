2cc5316d25f4a3c18569bc751e122011c59c41bf
Jonas Falck <jonas.falck@gmail.com>
2014-10-08 15:07:07 +0200

    started work on a53808

---
+++ eepa53808.go
@@ -0,0 +1,59 @@
+package goenocean
+
+type EepA53808 struct {
+	//*Telegram4bs
+	Telegram
+}
+
+// A5-38-08 CENTRAL COMMAND
+func NewEepA53808() *EepA53808 { // {{{
+	t := &EepA53808{NewTelegram4bs()}
+	//Must default to empty bytearray
+	t.SetTelegramData(make([]byte, 4))
+	return t
+} // }}}
+
+func (p *EepA53808) SetTelegram(t Telegram) { // {{{
+	p.Telegram = t
+} // }}}
+
+func (p *EepA53808) Command() uint8 {
+	return p.TelegramData()[0]
+}
+func (p *EepA53808) SetCommand(cmd uint8) {
+	tmp := p.TelegramData()
+	tmp[0] = cmd
+	p.SetTelegramData(tmp)
+}
+
+//func (p *EepA53808) TariffInfo() uint8 {
+//ti := (p.TelegramData()[3] & 0x0f0) >> 4
+//return ti
+//}
+
+//func (p *EepA53808) DataType() string {
+//d := (p.TelegramData()[3] >> 2) & 0x01
+
+//switch d {
+//case 0:
+//return "kWh"
+//case 1:
+//return "W"
+//}
+//return "Unknown"
+//}
+
+//func (p *EepA53808) dividor() int64 {
+//d := p.TelegramData()[3] & 0x03
+//switch d {
+//case 0:
+//return 1
+//case 1:
+//return 10
+//case 2:
+//return 100
+//case 3:
+//return 1000
+//}
+//return 1
+//}
